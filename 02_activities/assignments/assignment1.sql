2cbc01bb42cf93bc35c0f21e877a601d99fb5a37
klin093 <klin093@uottawa.ca>
2025-04-16 18:32:53 -0400

    Reformatted Queries
    Added actual column names.
    Clean up png

--- 02_activities/assignments/assignment1.sql
+++ 02_activities/assignments/assignment1.sql
@@ -4,24 +4,61 @@
 --SELECT
 /* 1. Write a query that returns everything in the customer table. */
 
-SELECT * FROM customer
+SELECT
+	customer_id,
+	customer_first_name,
+	customer_last_name,
+	customer_postal_code
+FROM
+	customer
 ;
 
-/* 2. Write a query that displays all of the columns and 10 rows from the cus- tomer table, 
+/* 2. Write a query that displays all of the columns and 10 rows from the customer table, 
 sorted by customer_last_name, then customer_first_ name. */
 
-SELECT * FROM customer ORDER BY customer_last_name ASC, customer_first_name ASC LIMIT 10
+SELECT
+	customer_id,
+	customer_first_name,
+	customer_last_name,
+	customer_postal_code
+FROM
+	customer
+ORDER BY customer_last_name ASC, customer_first_name ASC
+LIMIT 10
 ;
 
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. */
 -- option 1
-
-SELECT * FROM customer_purchases WHERE product_id IN (4,9)
+SELECT
+	product_id
+	vendor_id
+	market_date
+	customer_id
+	quantity
+	cost_to_customer_per_qty
+	transaction_time
+FROM
+	customer_purchases
+WHERE
+	product_id IN (4,9)
 ;
 
 -- option 2
 
+SELECT
+	product_id
+	vendor_id
+	market_date
+	customer_id
+	quantity
+	cost_to_customer_per_qty
+	transaction_time
+FROM
+	customer_purchases
+WHERE
+	product_id = 4 OR product_id = 9
+;
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
 filtered by vendor IDs between 8 and 10 (inclusive) using either:
@@ -33,10 +70,16 @@ filtered by vendor IDs between 8 and 10 (inclusive) using either:
 -- option 2
 
 SELECT
-	c.*,
-	c.quantity * c.cost_to_customer_per_qty AS price
+	product_id
+	vendor_id
+	market_date
+	customer_id
+	quantity
+	cost_to_customer_per_qty
+	transaction_time
+	quantity * cost_to_customer_per_qty AS price
 FROM
-	customer_purchases AS c
+	customer_purchases
 WHERE
 	vendor_id BETWEEN 8 AND 10
 ;
@@ -51,8 +94,8 @@ SELECT
 	product_id,
 	product_name,
 	CASE
-		WHEN product_qty_type <> 'unit' THEN 'bulk'
-		ELSE 'unit' END AS product_qty_type
+		WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk' END AS prod_qty_type_condensed
 FROM
 	product
 ;
@@ -65,13 +108,11 @@ SELECT
 	product_id,
 	product_name,
 	CASE
-		WHEN product_qty_type <> 'unit' THEN 'bulk'
-		ELSE 'unit'
-	END AS product_qty_type,
+		WHEN product_qty_type = 'unit' THEN 'unit'
+		ELSE 'bulk' END AS prod_qty_type_condensed
 	CASE
 		WHEN LOWER(product_name) LIKE '%pepper%' THEN 1
-		ELSE 0
-	END AS pepper_flag
+		ELSE 0 END AS pepper_flag
 FROM
 	product
 ;
@@ -81,7 +122,11 @@ FROM
 vendor_id field they both have in common, and sorts the result by vendor_name, then market_date. */
 
 SELECT
-	A.*,
+	A.vendor_id,
+	A.vendor_name,
+	A.vendor_type,
+	A.vendor_owner_first_name,
+	A.vendor_owner_last_name,
 	B.booth_number,
 	B.market_date
 FROM
